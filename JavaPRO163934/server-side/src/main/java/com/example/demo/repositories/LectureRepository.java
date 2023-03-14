28dd17840412775c53c6061a97aa815677a39dd0
Mariusz Dąbrowski <57808812+marioooo0o@users.noreply.github.com>
2023-03-14 17:26:57 +0100

    refactor: delete directory

--- JavaPRO163934/server-side/src/main/java/com/example/demo/repositories/LectureRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repositories;
-
-import com.example.demo.models.Lecture;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LectureRepository extends JpaRepository<Lecture, Long> {
-}
--- JavaPRO163934/server-side/src/main/java/com/example/demo/repositories/SlideRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repositories;
-
-import com.example.demo.models.Slide;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SlideRepository extends JpaRepository<Slide, Long> {
-}
